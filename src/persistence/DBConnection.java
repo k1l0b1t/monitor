01c4223ca7a81d2a5fe8e03cbf57ff45307d8d71
k1l0b1t <vincent@vincentlammens.be>
2020-09-02 16:17:15 +0200

    Removed file that is not needed anymore

--- src/persistence/DBConnection.java
+++
@@ -1,5 +0,0 @@
-package persistence;
-
-public class DBConnection {
-	public static final String JDBC_URL = "jdbc:mysql://root:root@localhost/monitor"; // this will load it from a configuration file, at some point;
-}
