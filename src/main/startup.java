237a20ba27112d68270c28e7e74178565e0583ba
Vincent Lammens <vincent@vincentlammens.be>
2021-01-30 10:47:48 +0100

    Formatted the code

--- src/main/startup.java
+++ src/main/startup.java
@@ -8,40 +8,44 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-public class startup {
+public class startup
+{
 
-	
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 
-		
-		final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
-	    executorService.scheduleAtFixedRate(new Runnable() {
-	        @Override
-	        public void run() {
-	            try {
+		final ScheduledExecutorService executorService = Executors
+			.newSingleThreadScheduledExecutor();
+		executorService.scheduleAtFixedRate(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				try
+				{
 					myTask();
-				} catch (IOException e) {
+				} catch (IOException e)
+				{
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-	        }
-	    }, 0, 1, TimeUnit.MINUTES); // the monitor update every minute
-			
+			}
+		}, 0, 1, TimeUnit.MINUTES); // the monitor update every minute
+
 	}
-	
-	
-	
-	private static void myTask() throws IOException {
-		
+
+	private static void myTask() throws IOException
+	{
+
 		// create new domain controller
 		DomainController dc = null;
 		dc = new DomainController();
-				
+
 		List<String> monitors = null;
 		monitors = dc.listMonitorNames();
-		
-		
-		for (String monitor : monitors) {
+
+		for (String monitor : monitors)
+		{
 			boolean status = false;
 			status = dc.getMonitorStatus(monitor);
 			System.out.printf("Monitor %s: %s%n", monitor, status);
